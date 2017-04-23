912d599e066a506cf7c7fc6d75f5d62fa88ca3ab
ruipinto <ruipinto@DESKTOP-A03587U.lan>
2017-04-23 22:35:44 +0100

    Create class Cão.

--- src/giteclipse/Cao.java
+++ src/giteclipse/Cao.java
@@ -1,5 +1,56 @@
 package giteclipse;
 
+import java.util.Scanner;
+
 public class Cao {
+	
+	String nome;
+	int idade;
+	
+	
+
+	public Cao() {
+		
+		System.out.println("Nome do cão: ");
+		Scanner keyboard = new Scanner(System.in);
+		String strNomeCao = keyboard.nextLine();
+		this.nome = strNomeCao;
+				
+		System.out.println("Idade do cão: ");
+		int intIdadeCao = keyboard.nextInt();
+		this.idade = intIdadeCao;
+		keyboard.close();
+			
+		
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	
+	
+	
 
 }
